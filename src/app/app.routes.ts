b64c0da1fb5ebda3de29816747ee08cacb834105
marcos <marcosmd1014@gmail.com>
2025-08-26 20:23:01 +0200

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { JornadaComponent } from './components/jornada/jornada.component';
+import { PartidoComponent } from './components/partido/partido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: JornadaComponent },
+  { path: 'partido/:id', component: PartidoComponent },
+  { path: '**', redirectTo: '' }
+];
